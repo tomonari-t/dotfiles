c3ca775f05262549bb69a00f4ce9be658a3a0d41
tomonari-t <tomonari@kyuzan.com>
2022-12-17 17:55:38 +0900

    Update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,14 @@
 local wezterm = require 'wezterm'
 
 return {
-  color_scheme = "gotham (Gogh)",
+  color_scheme = "nordfox",
+  font_size = 12.0,
+  use_ime = true,
+  animation_fps = 1,
+  cursor_blink_ease_in = "Constant",
+  cursor_blink_ease_out = "Constant",
+  cursor_blink_rate = 0,
+  exit_behavior = "CloseOnCleanExit",
+  tab_bar_at_bottom = false,
+  window_close_confirmation = "AlwaysPrompt",
 }
